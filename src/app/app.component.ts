cb627a95bf98d6bb05b74a314bb154c10de5604a
Brian Patrick <brian.patrick3@kyndryl.com>
2024-01-03 09:23:54 -0600

    add charts and icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,8 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
-import '@kyndryl-design-system/shidoka-applications/components/global/uiShell';
-import '@kyndryl-design-system/shidoka-applications/components/global/header';
-import '@kyndryl-design-system/shidoka-applications/components/global/localNav';
-import '@kyndryl-design-system/shidoka-applications/components/global/footer';
-
-// import SwitcherIcon from '!raw-loader?!@kyndryl-design-system/shidoka-foundation/assets/svg/switcher.svg';
-// import UserAvatar24 from '@carbon/icons/es/user--avatar/24';
-// import UserAvatar20 from '@carbon/icons/es/user--avatar/20';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
   title = 'shidoka-sample-angular';
