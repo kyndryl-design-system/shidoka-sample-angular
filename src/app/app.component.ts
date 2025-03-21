e2fa2906b27668bfe8c1e44f79fe9b3f55c63070
Brian Patrick <brian.patrick3@kyndryl.com>
2025-03-21 08:57:38 -0500

    fix: add meta color scheme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import userIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/user.svg';
-import questionIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/24/question.svg';
-import sampleIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/circle-stroke.svg'
+import userIcon20 from '@kyndryl-design-system/shidoka-icons/svg/monochrome/20/user.svg';
+import questionIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/20/question.svg';
+import sampleIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/circle-stroke.svg';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,4 +13,5 @@ export class AppComponent {
   userAvatar = userIcon;
   questionIcon = questionIcon;
   sampleIcon = sampleIcon;
+  userIcon20 = userIcon20;
 }
